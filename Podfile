98db52388a52ec57d4510a02d369c8b7fbacc8fc
silverframe <ibercombie@gmail.com>
2016-08-30 10:19:24 +0800

    Integrated Alamofire into project

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'top10' do
+  pod ‘Alamofire’, ‘~> 3.4’
+
+end
---
+++ Pods/Target Support Files/Pods-top10/Pods-top10-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_top10VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_top10VersionString[];
+
